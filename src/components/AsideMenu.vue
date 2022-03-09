a60279b6dd1d189b687bad3276c5f69c869c84c6
EgorKrab <100771269+EgorKrab@users.noreply.github.com>
2022-03-09 13:01:47 +0300

    Add files via upload

--- src/components/AsideMenu.vue
+++ src/components/AsideMenu.vue
@@ -5,7 +5,6 @@ import { useRouter } from 'vue-router'
 import ModalBox from '@/components/ModalBox.vue'
 import { DatePicker } from 'v-calendar'
 import { mdiMenu } from '@mdi/js'
-import JbButton from '@/components/JbButton.vue'
 import NavBarItem from '@/components/NavBarItem.vue'
 import Icon from '@/components/Icon.vue'
 
@@ -48,7 +47,6 @@ const isDark = computed(() => store.state.darkMode)
 const datePickerBG = computed(() => {
   return isDark.value ? 'rgb(31 41 55)' : 'rgb(243 244 246)'
 })
-
 const localization = computed(() => store.state.localization.localization)
 const attrs = computed(() => store.state.calendar.calendar)
 const user = computed(() => store.state.user.user)
@@ -67,7 +65,6 @@ const currentDate = computed({
     store.commit(TASK.CLEAN_UP_LOADED_TASKS)
   }
 })
-
 const navigatorMenu = reactive({
   tasks: false,
   foldableNavigator: false,
@@ -91,7 +88,6 @@ function dateToLabelFormat (calendarDate) {
 const asideLgClose = () => {
   store.dispatch('asideLgToggle', false)
 }
-
 const menuClick = (event, item) => {
   store.commit('updateLabel', item.label)
   if (UID_TO_ACTION[item.uid] && item.type === 'uid') {
@@ -111,51 +107,72 @@ const menuClick = (event, item) => {
   }
   store.commit(TASK.CLEAN_UP_LOADED_TASKS)
 }
+if (user.value.license_type === 0) {
+  document.getElementById('Lice').value = '234'
+}
 </script>
 
 <template>
   <!-- Profile modal -->
   <modal-box
     v-model="modalOneActive"
-    title="Profile"
-  >
+    title="Аккаунт и лицензия"
+    @logout="logout()"
+  ><form
+  class="rounded-b-3xl pb-7 mb-3 text-center"
+  :class="{'bg-[#FFF2E0]': user.license_type == 0} || {'bg-[#FFF2E0]': user.license_type == 1} || {'bg-[#FFF2E0]': user.license_type == 2} || {'bg-[#FFF2E0]': user.license_type == 3}">
     <img
-      :src="user.foto_link"
-      class="mx-auto rounded-full"
-    >
-    <strong>Current user: </strong>
-    <br>
-    <span>{{ user.current_user_name }}</span>
-    <br>
-    <span>{{ user.current_user_email }}</span>
-    <br>
-    <br>
+    :src="{'user.foto_link': user.foto_link == true} || {'':user.foto_link == false}"
+    class="mx-auto rounded-3xl content-center"
+  ><div>
+    <p>{{ user.current_user_name }}</p>
+    <a>{{ user.current_user_email }}</a>
+    </div>
+    <button type="submit" class="bg-[#403D38] rounded-xl px-10 p-3 text-white mt-4">
+      Изменить
+    </button>
+</form>
+<fieldset class="flex">
+<form class="mx-12 basis-9/10">
     <strong>{{ localization.owner_license }}</strong>
-    <br>
-    <span>{{ user.owner_title }}</span>
     <br>
     <span>{{ user.owner_email }}</span>
     <br>
     <br>
-    <strong>License expires</strong>
+    <strong>Тариф</strong>
     <br>
-    <span>{{ user.date_expired }}</span>
-    <br>
-    <br>
-    <strong>Days left</strong>
-    <br>
-    <span>{{ user.days_left }}</span>
+    <span id="Lice"
+    >{{ user.date_expired }}({{ user.days_left }})</span>
     <br>
     <br>
-    <strong>Used server storage </strong>
-    <span> {{ user.total_mb }} {{ localization.megabytes }} ({{ user.percent_mb }}%)</span>
+    <strong>Количество рабочих мест</strong>
+    <br>
+    <span>18</span>
     <br>
     <br>
-    <jb-button
-      label="Logout"
-      color="danger"
-      @click="modalOneActive = false; logout()"
-    />
+    <strong>Занято места на сервере</strong>
+    <br>
+    <span>{{ user.total_mb }} {{ localization.megabytes }} ({{ user.percent_mb }}%)</span>
+    <br>
+    <br>
+    <strong>Последняя синхронизация</strong>
+    <br>
+    <span>синхронизация</span>
+    <br>
+    <br>
+</form>
+<form class="mx-12 mt-3 basis-1/10">
+  <svg width="28" height="31" viewBox="0 0 28 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 0C0.895431 0 0 0.895431 0 2V3.92076C0 7.58112 2.17694 10.868 5.59249 12.2873C5.78016 14.4536 6.79357 16.4332 8.44504 17.8899C9.30831 18.6369 9.87132 19.6454 10.0965 20.7286H7.43164V24.4263H5.59249V29.0204H3.71582V31H24.2467V29.0204H22.4075V24.4263H20.5684V20.7286H17.9035C18.1287 19.6454 18.7292 18.6369 19.555 17.8899C21.2064 16.4706 22.2198 14.4536 22.4075 12.2873C25.7855 10.868 28 7.58112 28 3.92076V2C28 0.895431 27.1046 0 26 0H2ZM25.9357 1.97959V3.92076C25.9357 6.4606 24.5845 8.77634 22.37 10.0463V1.97959H25.9357ZM9.42091 24.4263V22.7455H18.5416V24.4263H9.42091ZM18.2038 16.3959C16.9276 17.5164 16.0643 19.0478 15.8391 20.7286H12.1609C11.9357 19.0478 11.0724 17.5164 9.79625 16.3959C8.40751 15.2007 7.6193 13.4452 7.6193 11.615V1.97959H20.4182V11.5776C20.3807 13.4452 19.555 15.2007 18.2038 16.3959ZM7.54424 29.0204V26.4059H20.3432V29.0204H7.54424ZM5.55496 10.0836C3.37802 8.81369 1.98928 6.4606 1.98928 3.92076V1.97959H5.55496V10.0836Z" fill="#FF9123"/>
+  </svg>
+</form>
+</fieldset>
+<form class="text-center" action="https://www.leadertask.ru/market">
+  <button type="submit" class="bg-[#FF9123] rounded-xl px-10 p-3 text-white">
+    Продлить лицензию
+  </button>
+</form>
+    <br>
   </modal-box>
   <!-- /Profile modal -->
 
--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -2,6 +2,7 @@
 import { mdiCog } from '@mdi/js'
 import { computed } from 'vue'
 import Icon from '@/components/Icon.vue'
+import { useStore } from 'vuex'
 
 const props = defineProps({
   title: {
@@ -26,9 +27,13 @@ const props = defineProps({
   hoverable: Boolean
 })
 
-const emit = defineEmits(['header-icon-click', 'submit'])
+const emit = defineEmits(['header-icon-click', 'header-icon2-click', 'submit'])
 
 const is = computed(() => props.form ? 'form' : 'div')
+
+const store = useStore()
+
+const user = computed(() => store.state.user.user)
 
 const componentClass = computed(() => {
   const base = [
@@ -47,7 +52,9 @@ const computedHeaderIcon = computed(() => props.headerIcon ?? mdiCog)
 const headerIconClick = () => {
   emit('header-icon-click')
 }
-
+const headerIcon2Click = () => {
+  emit('header-icon2-click')
+}
 const submit = e => {
   emit('submit', e)
 }
@@ -62,10 +69,22 @@ const submit = e => {
   >
     <header
       v-if="title"
-      class="flex items-stretch border-b border-gray-100 dark:border-gray-700"
+      :class="{'bg-[#FFF2E0]': user.license_type == 0} || {'bg-[#FFF2E0]': user.license_type == 1} || {'bg-[#FFF2E0]': user.license_type == 2} || {'bg-[#FFF2E0]': user.license_type == 3}"
+      class="flex items-stretch p-5 pt-12 dark:border-gray-700"
     >
+    <a
+        v-if="computedHeaderIcon"
+        href="#"
+        class="flex items-center py-3 px-4 justify-center ring-blue-700 focus:ring"
+        aria-label="more options"
+        @click.prevent="headerIconClick"
+      >
+      <svg width="14" height="22" viewBox="0 0 14 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12 2L2 10.64L11.2308 20" stroke="black" stroke-opacity="0.9" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </a>
       <p
-        class="flex items-center py-3 grow font-bold"
+        class=" text-center items-center py-3 grow font-bold"
         :class="[ icon ? 'px-4' : 'px-6' ]"
       >
         <icon
@@ -75,14 +94,16 @@ const submit = e => {
         />
         {{ title }}
       </p>
-      <a
-        v-if="computedHeaderIcon"
-        href="#"
-        class="flex items-center py-3 px-4 justify-center ring-blue-700 focus:ring"
-        aria-label="more options"
-        @click.prevent="headerIconClick"
-      >
-        <icon :path="computedHeaderIcon" />
+      <a v-if="computedHeaderIcon"
+      href="#"
+      class="flex items-center py-3 px-4 justify-center ring-blue-700 focus:ring"
+      aria-label="more options"
+      @click.prevent="headerIcon2Click"
+      @click="headerIconClick">
+      <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M13.0811 23.7778H3.40141C2.79878 23.7778 2.34681 23.2882 2.34681 22.7232V3.40144C2.34681 2.83648 2.83645 2.34684 3.40141 2.34684H13.0435C13.6461 2.34684 14.0981 1.85721 14.0981 1.29224C14.0981 0.727281 13.6461 0.199982 13.0811 0.199982H3.40141C1.63119 0.199982 0.199951 1.63122 0.199951 3.40144V22.6855C0.199951 24.4557 1.63119 25.887 3.40141 25.887H13.0435C13.6461 25.887 14.0981 25.3974 14.0981 24.8324C14.1357 24.2674 13.6461 23.7778 13.0811 23.7778Z" fill="#000000E5"/>
+        <path d="M25.6986 12.2902L19.1827 5.88728C18.7684 5.47297 18.0905 5.47297 17.6762 5.88728C17.2618 6.30159 17.2618 6.97954 17.6762 7.39385L22.3465 11.9889H9.84199C9.23937 11.9889 8.7874 12.4785 8.7874 13.0435C8.7874 13.6084 9.23937 14.1357 9.84199 14.1357H22.3089L17.6762 18.7308C17.2618 19.1451 17.2618 19.823 17.6762 20.2374C17.9021 20.4633 18.1658 20.5387 18.4294 20.5387C18.6931 20.5387 18.9567 20.4257 19.1827 20.2374L25.6986 13.7968C25.887 13.6084 26 13.3071 26 13.0435C26 12.7798 25.9246 12.5162 25.6986 12.2902Z" fill="#000000E5"/>
+        </svg>
       </a>
     </header>
     <div
@@ -93,7 +114,7 @@ const submit = e => {
     </div>
     <div
       v-else
-      :class="{'p-6':!hasTable}"
+      :class="{'pb-6':!hasTable}"
     >
       <slot />
     </div>

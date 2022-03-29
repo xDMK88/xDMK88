2ec60dfc39acbd8f05e5c3f0acce9ee4fbd6fc79
Dmitry <dmitry.maslov.almeza@gmail.com>
2022-03-29 17:37:30 +0300

    Добавил компоненты для отображения доски + драг-н-дроп

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-white shadow rounded px-3 pt-3 pb-5 border border-white">
+    <div class="flex justify-between">
+      <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">
+        {{ task.title }}
+      </p>
+      <img
+        class="w-6 h-6 rounded-full ml-3"
+        src="https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png"
+        alt="Avatar"
+      >
+    </div>
+    <div class="flex mt-4 justify-between items-center">
+      <span class="text-sm text-gray-600">{{ task.date }}</span>
+      <span class="text-sm text-gray-600">{{ task.type }}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>

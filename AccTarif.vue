e817eb556fd56467b6e8b0cf85cc562933b3db80
EgorKrab <100771269+EgorKrab@users.noreply.github.com>
2022-04-14 14:27:16 +0300

    Add files via upload

---
+++ AccTarif.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const localization = computed(() => store.state.localization.localization)
+
+const store = useStore()
+const user = computed(() => store.state.user.user)
+
+</script>
+<template>
+    <form class="px-5 text-lg">
+    <br>
+    <strong>{{ localization.owner_license }}</strong>
+    <br>
+    <span>{{ user.owner_title }}</span>
+    <br>
+    <br>
+    <strong>Почта владельца лицензии</strong>
+    <br>
+    <span>{{ user.owner_email }}</span>
+    <br>
+    <br>
+    <strong>Количество рабочих мест</strong>
+    <br>
+    <span>-</span>
+    <br>
+    <br>
+    <strong>Занято места на сервере</strong>
+    <br>
+    <span>{{ user.total_mb }} {{ localization.megabytes }} ({{ user.percent_mb }}%)</span>
+    <br>
+    <br>
+    <strong>Последняя синхронизация</strong>
+    <br>
+    <span>синхронизация</span>
+    <br>
+</form>
+    <form class="text-center text-lg" action="https://www.leadertask.ru/market">
+        <button type="submit" class="bg-gray-100 hover:bg-gray-200 rounded-xl px-6 p-1">
+          Продлить лицензию
+        </button>
+    </form>
+</template>
---
+++ ModalBox.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { computed } from 'vue'
+import JbButton from '@/components/JbButton.vue'
+import JbButtons from '@/components/JbButtons.vue'
+import CardComponent from '@/components/CardComponent.vue'
+import Divider from '@/components/Divider.vue'
+import Overlay from '@/components/Overlay.vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: null
+  },
+  largeTitle: {
+    type: String,
+    default: null
+  },
+  button: {
+    type: String,
+    default: 'info'
+  },
+  buttonLabel: {
+    type: String,
+    default: 'Done'
+  },
+  hasCancel: Boolean,
+  modelValue: {
+    type: [String, Number, Boolean],
+    default: null
+  },
+  hasButton: Boolean
+})
+
+const emit = defineEmits(['update:modelValue', 'cancel', 'acc', 'tarif', 'logout', 'confirm'])
+
+const value = computed({
+  get: () => props.modelValue,
+  set: value => emit('update:modelValue', value)
+})
+const accSelect = value => { emit(value) }
+const tarifSelect = value => { emit(value) }
+const logoutAcc = value => { emit(value) }
+const confirmCancel = mode => {
+  value.value = false
+  emit(mode)
+}
+
+const logout = () => logoutAcc('logout')
+const confirm = () => confirmCancel('confirm')
+const cancel = () => confirmCancel('cancel')
+const acc = () => accSelect('acc')
+const tarif = () => tarifSelect('tarif')
+</script>
+
+<template>
+  <overlay
+    v-show="value"
+    @overlay-click="cancel"
+  >
+    <card-component
+      v-show="value"
+      has-table
+      :title="title"
+      class="shadow-lg w-full md:w-3/5 lg:w-3/5 z-50  h-4/5"
+      @header-icon-click="cancel"
+      @header-icon2-click="logout"
+      @acc="acc"
+      @tarif="tarif"
+    >
+      <div class="space-y-3 items-center">
+        <h1
+          v-if="largeTitle"
+          class="text-2xl"
+        />
+        {{ largeTitle }}
+        <slot />
+      </div>
+
+      <divider
+        v-if="hasButton"
+      />
+
+      <jb-buttons>
+        <jb-button
+          v-if="hasButton"
+          :label="buttonLabel"
+          :color="button"
+          @click="confirm"
+        />
+        <jb-button
+          v-if="hasCancel"
+          label="Cancel"
+          :color="button"
+          outline
+          @click="cancel"
+        />
+      </jb-buttons>
+    </card-component>
+  </overlay>
+</template>

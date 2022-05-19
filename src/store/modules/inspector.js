cc8842ec2ac5c44b265ad59ab0f8a9209b8b1b97
olsoncarsen <gashilovdmitry@yandex.ru>
2022-05-19 16:56:51 +0300

    added karma request, remove z before merging inspector messages

--- src/store/modules/inspector.js
+++ src/store/modules/inspector.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { notify } from 'notiwind'
 
 const state = {
+  karma: []
 }
 
 const getters = {
@@ -59,10 +60,31 @@ const actions = {
           reject(err)
         })
     })
+  },
+  [INSPECTOR.KARMA_REQUEST]: ({ commit, dispatch }, uidUser) => {
+    return new Promise((resolve, reject) => {
+      const url = process.env.VUE_APP_INSPECTOR_API + 'karma?uid_user=' + uidUser
+      axios({ url: url, method: 'GET' })
+        .then(resp => {
+          commit(INSPECTOR.KARMA_REQUEST, resp.data)
+          resolve(resp)
+        }).catch(err => {
+          notify({
+            group: 'api',
+            title: 'INSPECTOR API Error, please make screenshot',
+            action: INSPECTOR.KARMA_REQUEST,
+            text: err.response.data
+          }, 15000)
+          reject(err)
+        })
+    })
   }
 }
 
 const mutations = {
+  [INSPECTOR.KARMA_REQUEST]: (state, karma) => {
+    state.karma = karma
+  }
 }
 
 export default {

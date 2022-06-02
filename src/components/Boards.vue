3ef5304897bcd2582ef7342fc1c205f4728ecb7f
Maslov Dmitry <dmitry.maslov.almeza@gmail.com>
2022-06-02 12:13:22 +0300

    Обновил раздел доски

--- src/components/Boards.vue
+++ src/components/Boards.vue
@@ -1,258 +1,203 @@
-<script setup>
-import { computed, ref } from 'vue'
-import Icon from '@/components/Icon.vue'
-import { useStore } from 'vuex'
-import properties from '@/icons/properties.js'
-import projectIcon from '@/icons/project.js'
-import sharedProject from '@/icons/shared_project.js'
-import gridView from '@/icons/grid-view.js'
-import listView from '@/icons/list-view.js'
-import * as BOARD from '@/store/actions/boards.js'
-import * as CARD from '@/store/actions/cards.js'
-import * as NAVIGATOR from '@/store/actions/navigator'
-import { SELECT_BOARD } from '@/store/actions/boards.js'
-import BoardModalBoxRename from '@/components/Board/BoardModalBoxRename.vue'
-const store = useStore()
-defineProps({
-  boards: {
-    type: Array,
-    default: () => []
-  }
-})
-// Serves as linkage between requests from storage and tree view navigator
-const UID_TO_ACTION = {
-  '2e8dddd0-125a-49ef-a87c-0ea17b1b7f56': CARD.BOARD_CARDS_REQUEST, // private
-  '1b30b42c-b77e-40a4-9b43-a19991809add': CARD.BOARD_CARDS_REQUEST // shared
-}
-
-const isGridView = computed(() => store.state.isGridView)
-const showAddColumn = ref(false)
-const updateGridView = (value) => {
-  store.commit('basic', { key: 'isGridView', value: value })
-  localStorage.setItem('isGridView', value)
-}
-
-const isPropertiesMobileExpanded = computed(() => store.state.isPropertiesMobileExpanded)
-const user = computed(() => store.state.user.user)
-const focusedBoard = ref('')
-function uuidv4 () {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  )
-}
-const openProperties = (board, parentBoardUid = '') => {
-  if (!isPropertiesMobileExpanded.value) {
-    store.dispatch('asidePropertiesToggle', true)
-  }
-
-  focusedBoard.value = board.uid
-  store.commit('basic', { key: 'propertiesState', value: 'board' })
-
-  // create empty instanse of project
-  if (!board || parentBoardUid) {
-    board = {
-      uid: uuidv4(),
-      uid_parent: parentBoardUid,
-      color: '',
-      comment: '',
-      plugin: '',
-      collapsed: 0,
-      isclosed: 0,
-      order: 0,
-      group: 0,
-      show: 0,
-      favorite: 0,
-      quiet: 0,
-      email_creator: user.value.current_user_email,
-      members: [user.value.current_user_uid], //  [user.value.current_user_email],
-      children: [],
-      name: '',
-      bold: 0,
-      type_access: 1
-    }
-  }
-  store.commit(BOARD.SELECT_BOARD, board)
-}
-
-const gotoChildren = (value) => {
-  if (UID_TO_ACTION[value.global_property_uid]) {
-    store.dispatch(UID_TO_ACTION[value.global_property_uid], value.uid)
-    store.commit('basic', { key: 'cardSource', value: { uid: value.global_property_uid, param: value.uid } })
-  }
-  // store.commit(CARD.CLEAN_UP_LOADED_CARDS)
-
-  const navElem = {
-    name: value.name,
-    key: 'greedSource',
-    uid: value.uid,
-    global_property_uid: value.global_property_uid,
-    greedPath: 'boards_children',
-    value: value.children
-  }
-
-  store.commit('pushIntoNavStack', navElem)
-  store.commit('basic', { key: 'greedSource', value: value.children })
-  store.commit('basic', { key: 'greedPath', value: 'boards_children' })
-}
-const onAddNewBoard = (name) => {
-  showAddColumn.value = ref(false)
-  store.commit('basic', { key: 'propertiesState', value: 'board' })
-  const data = {
-    uid: uuidv4(),
-    name: name !== '' ? name : 'Тест',
-    uid_parent: '00000000-0000-0000-0000-000000000000',
-    color: '',
-    comment: '',
-    plugin: '',
-    collapsed: 0,
-    isclosed: 0,
-    order: 0,
-    group: 0,
-    show: 0,
-    favorite: 0,
-    quiet: 0,
-    email_creator: user.value.current_user_email,
-    members: { [user.value.current_user_uid]: '1' },
-    children: [],
-    type_access: 1,
-    bold: 0
-  }
-  store.dispatch(BOARD.CREATE_BOARD_REQUEST, data).then(() => {
-  })
-  store.commit(NAVIGATOR.NAVIGATOR_PUSH_BOARD, [data])
-  store.commit(SELECT_BOARD, data)
-}
-</script>
-
-<template class="w-full">
-  <BoardModalBoxRename
-    v-model="showAddColumn"
-    v-show="showAddColumn"
-    :show="showAddColumn"
-    title="Добавить доску"
-    @cancel="showAddColumn = false"
-    @save="onAddNewBoard"
-  />
-  <div
-    v-for="(value, index) in boards"
-    :key="index"
-  >
-    <div
-      class="flex items-center w-full"
-      :class="{ 'justify-between': index == 0, 'mt-3': index == 1 } "
+<template>
+  <div class="w-full">
+    <BoardModalBoxRename
+      v-show="showAddBoard"
+      :show="showAddBoard"
+      title="Добавить доску"
+      @cancel="showAddBoard = false"
+      @save="onAddNewBoard"
+    />    <div
+      v-for="(value, index) in boards"
+      :key="index"
     >
-      <p class="text-2xl text-gray-800 font-bold second dark:text-gray-100">
-        {{ value.dep }}
-      </p>
       <div
-        v-if="index == 0"
-        class="flex"
+        class="flex items-center w-full"
+        :class="{ 'justify-between': index == 0, 'mt-[28px]': index == 1 }"
       >
-        <icon
-          :path="listView.path"
-          :width="listView.width"
-          :height="listView.height"
-          :box="listView.viewBox"
-          class="cursor-pointer hover:text-gray-800 mr-2 mt-0.5"
-          :class="{ 'text-gray-800': !isGridView, 'text-gray-400': isGridView }"
-          @click="updateGridView(false)"
-        />
-        <icon
-          :path="gridView.path"
-          :width="gridView.width"
-          :height="gridView.height"
-          :box="gridView.viewBox"
-          class="cursor-pointer hover:text-gray-800 mr-2 mt-0.5"
-          :class="{ 'text-gray-800': isGridView, 'text-gray-400': !isGridView }"
-          @click="updateGridView(true)"
-        />
-      </div>
-    </div>
-    <div
-      class="grid gap-4 mt-3"
-      :class="{ 'md:grid-cols-2 lg:grid-cols-4': isGridView, 'grid-cols-1': !isGridView, 'grid-cols-1': isPropertiesMobileExpanded && !isGridView, 'lg:grid-cols-2': isPropertiesMobileExpanded && isGridView }"
-    >
-      <template
-        v-for="(board, pindex) in value.items"
-        :key="pindex"
-      >
+        <p class="font-['Roboto'] text-[#424242] text-[19px] leading-[22px] font-bold">
+          {{ value.dep }}
+        </p>
         <div
-          class="flex items-center bg-white dark:bg-gray-900 rounded-xl shadow hover:shadow-md px-5 relative min-h-[80px] cursor-pointer"
-          :class="{ 'ring-4 ring-orange-300': focusedBoard == board.uid }"
-          @click.stop="gotoChildren(board)"
+          v-if="index == 0"
+          class="flex"
         >
           <icon
-            :path="properties.path"
-            :width="properties.width"
-            :height="properties.height"
-            :box="properties.viewBox"
-            class="text-gray-400 cursor-pointer hover:text-gray-800 absolute top-8 right-4"
-            @click.stop="openProperties(board)"
+            :path="listView.path"
+            :width="listView.width"
+            :height="listView.height"
+            :box="listView.viewBox"
+            class="cursor-pointer hover:text-gray-800 mr-2"
+            :class="{
+              'text-gray-800': !isGridView,
+              'text-gray-400': isGridView
+            }"
+            @click="updateGridView(false)"
           />
-          <div
-            v-if="board.color != '#A998B6'"
-            :style="{ backgroundColor: board.color }"
-            class="w-full h-4 absolute top-0 right-0 rounded-t-xl"
+          <icon
+            :path="gridView.path"
+            :width="gridView.width"
+            :height="gridView.height"
+            :box="gridView.viewBox"
+            class="cursor-pointer hover:text-gray-800 mr-2"
+            :class="{
+              'text-gray-800': isGridView,
+              'text-gray-400': !isGridView
+            }"
+            @click="updateGridView(true)"
           />
-          <div class="relative">
-            <div
-              v-if="board.children && board.children.length"
-              class="min-w-[15px] min-h-[15px] absolute rounded-full bg-gray-500 -top-2 -left-2 flex items-center justify-center"
-            >
-              <span
-                class="text-white text-xs"
-              >
-                {{ board.children.length }}
-              </span>
-            </div>
-            <div class="flex items-start">
-              <icon
-                v-if="board.members && Object.keys(board.members).length == 1"
-                :path="projectIcon.path"
-                :width="18"
-                :height="18"
-                :box="projectIcon.viewBox"
-                class="text-gray-500 mr-2 mt-0.5"
-              />
-              <icon
-                v-if="board.members && Object.keys(board.members).length > 1"
-                :path="sharedProject.path"
-                :width="18"
-                :height="18"
-                :box="sharedProject.viewBox"
-                class="text-gray-500 mr-2 mt-0.5"
-              />
-              <p
-                class="font-normal"
-              >
-                {{ board.name.length > 60 ? board.name.substring(0, 60) + '...' : board.name }}
-              </p>
-            </div>
-          </div>
         </div>
-      </template>
+      </div>
       <div
-        v-if="index == 0"
-        class="flex items-center justify-center bg-gray-50 dark:bg-gray-900 rounded-xl border-2 border-dashed border-gray-300 dark:border-gray-500 cursor-pointer px-5 min-h-[80px]"
-        @click="showAddColumn = true"
+        class="grid gap-2 mt-3"
+        :class="{
+          'md:grid-cols-2 lg:grid-cols-4': isGridView,
+          'grid-cols-1': !isGridView,
+          'grid-cols-1': isPropertiesMobileExpanded && !isGridView,
+          'lg:grid-cols-2': isPropertiesMobileExpanded && isGridView
+        }"
       >
-        <div class="flex items-center justify-center w-10 h-10 bg-gray-200 dark:bg-gray-800 rounded-xl text-gray-100">
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M8.00011 2.3457V8.4034M8.00011 8.4034V14.4611M8.00011 8.4034H14.4617M8.00011 8.4034H1.53857"
-              stroke="#3E3D3B"
-              stroke-width="3"
-              stroke-linecap="round"
-            />
-          </svg>
-        </div>
+        <template
+          v-for="board in value.items"
+          :key="board.uid"
+        >
+          <BoardBlocItem
+            :board="board"
+            @click.stop="gotoChildren(board)"
+          />
+        </template>
+        <BoardBlocAdd
+          v-if="index == 0"
+          @click.stop="showAddBoard = true"
+        />
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import Icon from '@/components/Icon.vue'
+import BoardModalBoxRename from '@/components/Board/BoardModalBoxRename.vue'
+import BoardBlocItem from '@/components/Board/BoardBlocItem.vue'
+import BoardBlocAdd from '@/components/Board/BoardBlocAdd.vue'
+import * as CARD from '@/store/actions/cards.js'
+import * as BOARD from '@/store/actions/boards'
+import * as NAVIGATOR from '@/store/actions/navigator'
+
+import gridView from '@/icons/grid-view.js'
+import listView from '@/icons/list-view.js'
+
+export default {
+  components: {
+    Icon,
+    BoardModalBoxRename,
+    BoardBlocItem,
+    BoardBlocAdd
+  },
+  props: {
+    boards: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      showAddBoard: false,
+      gridView,
+      listView
+    }
+  },
+  computed: {
+    isGridView () {
+      return this.$store.state.isGridView
+    },
+    isPropertiesMobileExpanded () {
+      return this.$store.state.isPropertiesMobileExpanded
+    }
+  },
+  methods: {
+    print (val) {
+      console.log(val)
+    },
+    updateGridView (value) {
+      this.$store.commit('basic', { key: 'isGridView', value: value })
+      localStorage.setItem('isGridView', value)
+    },
+    gotoChildren (board) {
+      this.$store.dispatch(CARD.BOARD_CARDS_REQUEST, board.uid)
+      this.$store.commit('basic', {
+        key: 'cardSource',
+        value: { uid: board.global_property_uid, param: board.uid }
+      })
+      // store.commit(TASK.CLEAN_UP_LOADED_TASKS)
+
+      const navElem = {
+        name: board.name,
+        key: 'greedSource',
+        uid: board.uid,
+        global_property_uid: board.global_property_uid,
+        greedPath: 'boards_children',
+        value: board.children
+      }
+
+      this.$store.commit('pushIntoNavStack', navElem)
+      this.$store.commit('basic', { key: 'greedSource', value: board.children })
+      this.$store.commit('basic', {
+        key: 'greedPath',
+        value: 'boards_children'
+      })
+    },
+    uuidv4 () {
+      return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+        (
+          c ^
+          (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+        ).toString(16)
+      )
+    },
+    onAddNewBoard (name) {
+      this.showAddBoard = false
+      const title = name.trim()
+      if (title) {
+        // добавляем новую доску и переходим в неё
+        const maxOrder =
+          this.boards[0]?.items?.reduce(
+            (maxOrder, child) =>
+              child.order > maxOrder ? child.order : maxOrder,
+            0
+          ) || 0
+        const user = this.$store.state.user.user
+        const members = {}
+        members[user.current_user_uid] = 1
+        const board = {
+          uid: this.uuidv4(),
+          name: title,
+          uid_parent: '00000000-0000-0000-0000-000000000000',
+          email_creator: user.current_user_email,
+          order: maxOrder + 1,
+          collapsed: 0,
+          color: '',
+          public_link_status: 0,
+          show_date: 0,
+          favorite: 0,
+          stages: [],
+          children: [],
+          members
+        }
+        console.log(`create board uid: ${board.uid}`, board)
+        this.$store.dispatch(BOARD.CREATE_BOARD_REQUEST, board).then((res) => {
+          // заполняем недостающие параметры
+          board.global_property_uid = '1b30b42c-b77e-40a4-9b43-a19991809add'
+          board.type_access = res.data.type_access
+          board.color = '#A998B6'
+          //
+          this.$store.commit(BOARD.PUSH_BOARD, [board])
+          this.$store.commit(NAVIGATOR.NAVIGATOR_PUSH_BOARD, [board])
+          this.gotoChildren(board)
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped></style>

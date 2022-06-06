a8b7189573f3901a2e7de507e212c624ddc58150
Gashilov Dmitry <gashilovdmitry@yandex.ru>
2022-06-06 10:08:59 +0300

    fixed localStorage overflow bug

--- src/store/helpers/functions.js
+++ src/store/helpers/functions.js
@@ -18,13 +18,27 @@ export function showNotify (notification, notificationSound = true) {
   }
 }
 
+function wipeCachedFilesFromLocalStorage () {
+  const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi
+  for (const key in localStorage) {
+    if (regexExp.test(key)) {
+      localStorage.removeItem(key)
+    }
+  }
+}
+
 export function writeCache (key, blob) {
   if (blob.size > 2_000_000) return // Don't cache if blob size is over 2M
   const reader = new FileReader()
   reader.readAsDataURL(blob)
   reader.onloadend = function () {
     const base64data = reader.result
-    localStorage.setItem(key, base64data)
+    try {
+      localStorage.setItem(key, base64data)
+    } catch (e) {
+      wipeCachedFilesFromLocalStorage()
+      localStorage.setItem(key, base64data)
+    }
   }
 }
 

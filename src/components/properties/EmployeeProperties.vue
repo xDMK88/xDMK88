8c626b6fda8475d938f118de96bd8d0bd85ec3c8
Maslov Dmitry <dmitry.maslov.almeza@gmail.com>
2022-06-08 18:07:56 +0300

    Отображение отделов в свойствах сотрудника

--- src/components/properties/EmployeeProperties.vue
+++ src/components/properties/EmployeeProperties.vue
@@ -3,9 +3,9 @@
     <ModalBoxDelete
       v-show="showConfirm"
       title="Удалить сотрудника"
-      :text="`Вы действительно хотите удалить сотрудника ${selectedEmployee.name}?`"
+      :text="`Вы действительно хотите удалить сотрудника ${selectedEmployeeName}?`"
       @cancel="showConfirm = false"
-      @yes="removeEmployee(selectedEmployee)"
+      @yes="removeEmployee"
     />
     <div class="flex justify-end">
       <div
@@ -55,8 +55,12 @@
       </div>
     </div>
     <div class="mt-[20px] flex gap-[4px]">
-      <div class="grow flex items-center gap-[4px] pl-[5px] pr-[9px] h-[30px] cursor-pointer bg-[#f4f5f7] rounded-[6px] font-roboto text-[#575758] text-[12px] font-medium">
+      <div
+        class="group grow flex items-center gap-[4px] pl-[5px] pr-[9px] h-[30px] bg-[#f4f5f7] rounded-[6px] font-roboto text-[#575758] text-[12px] font-medium"
+        :class="{'cursor-pointer': isCanChangeDepartments}"
+      >
         <svg
+          :class="{'group-hover:hidden': selectedEmployeeDep && isCanChangeDepartments}"
           width="20"
           height="20"
           viewBox="0 0 30 30"
@@ -68,11 +72,24 @@
             fill="#606061"
           />
         </svg>
-        Отдел
+        <svg
+          v-if="selectedEmployeeDep && isCanChangeDepartments"
+          class="hidden"
+          :class="{'group-hover:block': selectedEmployeeDep && isCanChangeDepartments}"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11.1798 10.0034L14.7632 6.42841C14.9201 6.27149 15.0082 6.05866 15.0082 5.83674C15.0082 5.61483 14.9201 5.402 14.7632 5.24508C14.6062 5.08816 14.3934 5 14.1715 5C13.9496 5 13.7368 5.08816 13.5798 5.24508L10.0048 8.82841L6.42983 5.24508C6.27291 5.08816 6.06008 5 5.83817 5C5.61625 5 5.40342 5.08816 5.2465 5.24508C5.08958 5.402 5.00142 5.61483 5.00142 5.83674C5.00142 6.05866 5.08958 6.27149 5.2465 6.42841L8.82983 10.0034L5.2465 13.5784C5.16839 13.6559 5.1064 13.748 5.06409 13.8496C5.02178 13.9511 5 14.0601 5 14.1701C5 14.2801 5.02178 14.389 5.06409 14.4906C5.1064 14.5921 5.16839 14.6843 5.2465 14.7617C5.32397 14.8399 5.41614 14.9018 5.51768 14.9442C5.61923 14.9865 5.72816 15.0082 5.83817 15.0082C5.94818 15.0082 6.0571 14.9865 6.15865 14.9442C6.2602 14.9018 6.35236 14.8399 6.42983 14.7617L10.0048 11.1784L13.5798 14.7617C13.6573 14.8399 13.7495 14.9018 13.851 14.9442C13.9526 14.9865 14.0615 15.0082 14.1715 15.0082C14.2815 15.0082 14.3904 14.9865 14.492 14.9442C14.5935 14.9018 14.6857 14.8399 14.7632 14.7617C14.8413 14.6843 14.9033 14.5921 14.9456 14.4906C14.9879 14.389 15.0097 14.2801 15.0097 14.1701C15.0097 14.0601 14.9879 13.9511 14.9456 13.8496C14.9033 13.748 14.8413 13.6559 14.7632 13.5784L11.1798 10.0034Z"
+            fill="#7E7E80"
+          />
+        </svg>
+        {{ selectedEmployeeDep || 'Отдел' }}
       </div>
-      <PopMenu
-        v-if="selectedEmployee.uid && employees[user.current_user_uid].type != 3 && selectedEmployee.type != 1"
-      >
+      <PopMenu v-if="isCanDeleteEmp">
         <div class="flex-none flex items-center justify-center h-[30px] w-[30px] bg-[#F4F5F7] rounded-[6px] cursor-pointer">
           <svg
             width="20"
@@ -128,17 +145,15 @@ export default {
     }
   },
   computed: {
-    employees () {
-      return this.$store.state.employees.employees
-    },
     selectedEmployee () {
       return this.$store.state.employees.selectedEmployee
     },
     selectedEmployeeName () {
-      return this.$store.state.employees.selectedEmployee?.name || ''
+      return this.selectedEmployee?.name || ''
     },
-    user () {
-      return this.$store.state.user.user
+    selectedEmployeeDep () {
+      const dep = this.$store.state.departments.deps[this.selectedEmployee?.uid_dep]
+      return dep?.name || ''
     },
     empFoto () {
       const fotolink = this.selectedEmployee?.fotolink
@@ -148,6 +163,26 @@ export default {
         return foto + '&size=l'
       }
       return fotolink
+    },
+    isSelectedEmployeeCurrentUser () {
+      const user = this.$store.state.user.user
+      return user.current_user_uid === this.selectedEmployee.uid
+    },
+    isCanChangeDepartments () {
+      const employees = this.$store.state.employees.employees
+      const user = this.$store.state.user.user
+      const userType = employees[user.current_user_uid].type
+      return userType === 1
+    },
+    isCanDeleteEmp () {
+      const employees = this.$store.state.employees.employees
+      const user = this.$store.state.user.user
+      const userType = employees[user.current_user_uid].type
+      const userAdmin = userType === 1 || userType === 2
+      // текущий пользователь админ
+      // тот которого удаляем не суперадмин
+      // тот которого удаляем не текущий пользователь
+      return userAdmin && this.selectedEmployee.type !== 1 && !this.isSelectedEmployeeCurrentUser
     }
   },
   watch: {
@@ -159,12 +194,15 @@ export default {
     }
   },
   methods: {
-    removeEmployee (employee) {
+    print (msg, param) {
+      console.log(msg, param)
+    },
+    removeEmployee () {
       this.showConfirm = false
-      this.$store.dispatch(EMPLOYEE.REMOVE_EMPLOYEE_REQUEST, employee)
+      this.$store.dispatch(EMPLOYEE.REMOVE_EMPLOYEE_REQUEST, this.selectedEmployee)
         .then(() => {
           this.$store.dispatch('asidePropertiesToggle', false)
-          this.$store.commit(NAVIGATOR_REMOVE_EMPLOYEE, employee)
+          this.$store.commit(NAVIGATOR_REMOVE_EMPLOYEE, this.selectedEmployee)
         })
     },
     closeProperties () {

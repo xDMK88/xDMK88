8771e2ba5004de899229348e910801c9bd5951e7
Maslov Dmitry <dmitry.maslov.almeza@gmail.com>
2022-06-08 17:03:09 +0300

    Свойства сотрудника в дизайне

--- src/store/modules/employees.js
+++ src/store/modules/employees.js
@@ -1,10 +1,7 @@
 import { NAVIGATOR_PUSH_EMPLOYEE } from '@/store/actions/navigator'
 import axios from 'axios'
 import { notify } from 'notiwind'
-import {
-  CREATE_EMPLOYEE_REQUEST, PUSH_EMPLOYEE,
-  PUSH_EMPLOYEE_BY_EMAIL, REMOVE_EMPLOYEE_REQUEST, SELECT_EMPLOYEE, UPDATE_EMPLOYEE_REQUEST
-} from '../actions/employees'
+import * as EMPLOYEE from '../actions/employees'
 
 const state = {
   employees: {},
@@ -15,15 +12,15 @@ const state = {
 const getters = {}
 
 const actions = {
-  [CREATE_EMPLOYEE_REQUEST]: ({ commit, dispatch }, data) => {
+  [EMPLOYEE.CREATE_EMPLOYEE_REQUEST]: ({ commit, dispatch }, data) => {
     return new Promise((resolve, reject) => {
       const url = process.env.VUE_APP_LEADERTASK_API + 'api/v1/emp'
       axios({ url: url, method: 'POST', data: data })
         .then((resp) => {
           const employee = resp.data
           employee.parentID = 'd28e3872-9a23-4158-aea0-246e2874da73' // resp.data.emps.uid
-          commit(PUSH_EMPLOYEE, employee)
-          commit(PUSH_EMPLOYEE_BY_EMAIL, employee)
+          commit(EMPLOYEE.PUSH_EMPLOYEE, employee)
+          commit(EMPLOYEE.PUSH_EMPLOYEE_BY_EMAIL, employee)
           commit(NAVIGATOR_PUSH_EMPLOYEE, [employee])
           resolve(resp)
         })
@@ -32,7 +29,7 @@ const actions = {
             {
               group: 'api',
               title: 'REST API Error, please make screenshot',
-              action: CREATE_EMPLOYEE_REQUEST,
+              action: EMPLOYEE.CREATE_EMPLOYEE_REQUEST,
               text: err.response?.data ?? err
             },
             15000
@@ -41,7 +38,7 @@ const actions = {
         })
     })
   },
-  [UPDATE_EMPLOYEE_REQUEST]: ({ commit, dispatch }, data) => {
+  [EMPLOYEE.CHANGE_EMPLOYEE_NAME]: ({ commit, dispatch }, data) => {
     return new Promise((resolve, reject) => {
       const url =
         process.env.VUE_APP_LEADERTASK_API +
@@ -51,6 +48,7 @@ const actions = {
         data.name
       axios({ url: url, method: 'PATCH' })
         .then((resp) => {
+          commit('ChangeEmployeeName', data)
           resolve(resp)
         })
         .catch((err) => {
@@ -58,8 +56,8 @@ const actions = {
             {
               group: 'api',
               title: 'REST API Error, please make screenshot',
-              action: UPDATE_EMPLOYEE_REQUEST,
-              text: err.response.data
+              action: EMPLOYEE.CHANGE_EMPLOYEE_NAME,
+              text: err.response?.data || err
             },
             15000
           )
@@ -67,7 +65,7 @@ const actions = {
         })
     })
   },
-  [REMOVE_EMPLOYEE_REQUEST]: ({ commit, dispatch }, data) => {
+  [EMPLOYEE.REMOVE_EMPLOYEE_REQUEST]: ({ commit, dispatch }, data) => {
     return new Promise((resolve, reject) => {
       const url =
         process.env.VUE_APP_LEADERTASK_API +
@@ -83,7 +81,7 @@ const actions = {
             {
               group: 'api',
               title: 'REST API Error, please make screenshot',
-              action: REMOVE_EMPLOYEE_REQUEST,
+              action: EMPLOYEE.REMOVE_EMPLOYEE_REQUEST,
               text: err.response.data
             },
             15000
@@ -95,14 +93,18 @@ const actions = {
 }
 
 const mutations = {
-  [PUSH_EMPLOYEE]: (state, employee) => {
+  [EMPLOYEE.PUSH_EMPLOYEE]: (state, employee) => {
     state.employees[employee.uid] = employee
   },
-  [PUSH_EMPLOYEE_BY_EMAIL]: (state, employee) => {
+  [EMPLOYEE.PUSH_EMPLOYEE_BY_EMAIL]: (state, employee) => {
     state.employeesByEmail[employee.email.toLowerCase()] = employee
   },
-  [SELECT_EMPLOYEE]: (state, employee) => {
+  [EMPLOYEE.SELECT_EMPLOYEE]: (state, employee) => {
     state.selectedEmployee = employee
+  },
+  ChangeEmployeeName: (state, data) => {
+    const employee = state.employeesByEmail[data.email.toLowerCase()]
+    employee.name = data.name
   }
 }
 

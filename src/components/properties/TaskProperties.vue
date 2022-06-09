af81672cc1dd0da72bae4825b5b8e9c14aff2950
kakeoff <jasonsuper04@gmail.com>
2022-06-09 11:25:00 +0300

    фикс отображение кнопок у задач в доступе

--- src/components/properties/TaskProperties.vue
+++ src/components/properties/TaskProperties.vue
@@ -1303,7 +1303,7 @@ export default {
         />
         <!-- Кнопка Выбрать дату -->
         <TaskPropsButtonSetDate
-          v-if="!((selectedTask.uid_customer !== user.current_user_uid) && (selectedTask.status === 1))"
+          v-if="(selectedTask.type === 1 || selectedTask.type === 2) && !((selectedTask.uid_customer !== user.current_user_uid) && (selectedTask.status === 1))"
           :date-begin="selectedTask.date_begin"
           :date-end="selectedTask.date_end"
           :date-text="selectedTask.term_user"
@@ -1877,14 +1877,14 @@ export default {
          </Popper>-->
         <!-- Кнопка Проект -->
         <TaskPropsButtonProject
-          v-if="(selectedTask.type === 1 || selectedTask.type === 2 || (selectedTask.uid_project !== '00000000-0000-0000-0000-000000000000')) && !((selectedTask.uid_customer !== user.current_user_uid) && (selectedTask.status === 1))"
+          v-if="(selectedTask.uid_project !== '00000000-0000-0000-0000-000000000000') && !((selectedTask.uid_customer !== user.current_user_uid) && (selectedTask.status === 1))"
           :selected-project="selectedTask.uid_project"
-          :can-edit="selectedTask.type === 1 || selectedTask.type === 2"
+          :can-edit="selectedTask.uid_customer === user.current_user_uid"
           @changeProject="onChangeProject"
         />
         <!-- Кнопка Цвет -->
         <TaskPropsButtonColor
-          v-if="(selectedTask.type === 1 || selectedTask.type === 2 || (selectedTask.uid_project !== '00000000-0000-0000-0000-000000000000')) && !((selectedTask.uid_customer !== user.current_user_uid) && (selectedTask.status === 1))"
+          v-if="(selectedTask.type === 1 || selectedTask.type === 2) && !((selectedTask.uid_customer !== user.current_user_uid) && (selectedTask.status === 1))"
           :selected-color="selectedTask.uid_marker"
           :can-edit="selectedTask.type === 1 || selectedTask.type === 2"
           @changeColor="onChangeColor"

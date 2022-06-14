baef15780037a2afbd89e031396ee78d28d1d6ee
Bukharin Maxim <jojojopa@icloud.com>
2022-06-14 17:51:27 +0300

    фикс логики отображения заказчика/исполнителя

--- src/components/Doitnow/DoitnowTask.vue
+++ src/components/Doitnow/DoitnowTask.vue
@@ -74,13 +74,13 @@
     <div class="flex text-sm text-left justify-between w-[200px]">
       <div class="flex flex-col" style="color: #7E7E80">
         <span
-          v-show="task.uid_customer !== task.uid_performer"
+          v-show="(task.uid_customer !== user.current_user_uid) && (task.uid_performer !== user.current_user_uid)"
           class="mb-2 w-[100px]"
         >
           Заказчик:
         </span>
         <span
-          v-show="task.uid_customer !== task.uid_performer"
+          v-show="(task.uid_customer !== user.current_user_uid) && (task.uid_performer !== user.current_user_uid)"
           class="mb-2 w-[100px]"
         >
           Исполнитель:

8bcb65925a518448b01f104255783df5968272d3
Bukharin Maxim <jojojopa@icloud.com>
2022-06-20 10:19:36 +0300

    новая директория для раздела "цвета"

--- src/components/Colors/ColorBlock.vue
+++ src/components/Colors/ColorBlock.vue
@@ -3,11 +3,9 @@
     class="flex items-center bg-white rounded-[8px] shadow hover:shadow-md px-[15px] relative h-[48px] cursor-pointer"
     :style="{ backgroundColor: getValidBackColor(color.back_color) }"
   >
-    <div
-      class="w-full h-[48px] absolute top-0 right-0 rounded-t-[8px]"
-    />
     <div class="w-full">
       <div class="flex items-center relative">
+        <ColorIcon :color="getValidForeColor(color.fore_color)" />
         <p
           class="flex-initial ml-[10px] font-['Roboto'] text-[13px] leading-[15px] font-medium truncate w-full"
           :style="{ color: getValidForeColor(color.fore_color) }"
@@ -20,7 +18,12 @@
 </template>
 
 <script>
+import ColorIcon from '@/components/Colors/Icons/ColorIcon.vue'
+
 export default {
+  components: {
+    ColorIcon
+  },
   props: {
     color: {
       type: Object,
